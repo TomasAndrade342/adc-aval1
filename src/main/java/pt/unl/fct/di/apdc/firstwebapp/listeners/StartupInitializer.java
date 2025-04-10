970b0de1252748447525e4b6833b5482fc7cbdce
Tomas Andrade <tfo.andrade@campus.fct.unl.pt>
2025-04-10 10:58:55 +0100

    Added root user creation, to test

---
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/listeners/StartupInitializer.java
@@ -0,0 +1,36 @@
+package pt.unl.fct.di.apdc.firstwebapp.listeners;
+
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.Key;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import com.google.cloud.datastore.Datastore;
+import org.apache.commons.codec.digest.DigestUtils;
+
+@WebListener
+public class StartupInitializer implements ServletContextListener {
+    private static final Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+
+    @Override
+    public void contextInitialized(ServletContextEvent event) {
+        Key rootKey = datastore.newKeyFactory().setKind("User").newKey("root");
+
+        if (datastore.get(rootKey) == null) {
+            Entity rootUser = Entity.newBuilder(rootKey)
+                    .set("email", "root@admin.pt")
+                    .set("username", "root")
+                    .set("nome", "System Root Administrator")
+                    .set("telefone", "+123456789")
+                    .set("password", DigestUtils.sha512Hex("Root123!!"))
+                    .set("perfil", "private")
+                    .set("role", "admin")
+                    .set("estado", "active")
+                    .build();
+
+            datastore.put(rootUser);
+            System.out.println("Conta root criada com sucesso.");
+        }
+    }
+}
---
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/UserResource.java
@@ -0,0 +1,28 @@
+package pt.unl.fct.di.apdc.firstwebapp.resources;
+
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.gson.Gson;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+import java.util.logging.Logger;
+
+@Path("/users")
+public class UserResource {
+
+    private static final Logger LOG = Logger.getLogger(RegisterResource.class.getName());
+    private static final Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+
+    private final Gson g = new Gson();
+
+    public UserResource() {}
+
+    @POST
+    @Path("register")
+    public Response registerUser() {
+
+        return null;
+    }
+}
